b7fb412ee3d705ae4f012e5182d7d44a0b4448a8
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2022-09-22 16:12:02 -0300

    banco de dados sqlite

---
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/model/AppDatabase.kt
@@ -0,0 +1,9 @@
+package co.tiagoaguiar.fitnesstracker.model
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Calc::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract  fun calcDao(): CalcDao
+}
